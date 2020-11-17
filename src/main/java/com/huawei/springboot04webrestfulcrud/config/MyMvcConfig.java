92464961c528046e5f88cac4451855d7ee459565
wangshoujia <jia825218328@163.com>
2020-11-17 20:51:56 +0800

    testforbranch_2

---
+++ src/main/java/com/huawei/springboot04webrestfulcrud/config/MyMvcConfig.java
@@ -0,0 +1,18 @@
+package com.huawei.springboot04webrestfulcrud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/atguigu").setViewName("success");
+
+    }
+
+
+}
