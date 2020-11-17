9fd4b18b510cf4a138e538e5dce6a88ef33691bf
wangshoujia <jia825218328@163.com>
2020-11-17 20:41:03 +0800

    testforbranch

---
+++ src/main/java/com/huawei/springboot04webrestfulcrud/Springboot04WebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.springboot04webrestfulcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04WebRestfulcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04WebRestfulcrudApplication.class, args);
+	}
+
+}
